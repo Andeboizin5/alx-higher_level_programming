7d53c15602293c03b2142d25f6b06980cd0061f9
Andeboizin5 <andeboizin5@gmail.com>
2024-02-05 11:44:02 -0800

    2-is_same_class.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,4 +6,4 @@ class MyList(list):
     def print_sorted(self):
         """Prints the list sorted in ascending order."""
         sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self)
