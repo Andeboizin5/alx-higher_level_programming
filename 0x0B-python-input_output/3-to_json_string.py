5b2a0e2a7d3eaacce61fa650ad5b410e8840bed5
Andeboizin5 <andeboizin5@gmail.com>
2024-02-06 04:47:03 -0800

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""
-Module for converting an object to a JSON string.
-"""
+"""Module for converting an object to a JSON string."""
+import json
 
 
 def to_json_string(my_obj):
-    """
-    Returns the JSON representation of an object (string).
-
-    Args:
-        my_obj: The object to be converted to JSON.
-
-    Returns:
-        str: The JSON string representation of the object.
-    """
-    import json
+    """Returns the JSON representation of an object (string."""
     return json.dumps(my_obj)
-
 
